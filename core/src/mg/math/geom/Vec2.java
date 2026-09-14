f1eb74b9135d2ba7cdff6818b09d7494363373f9
jstation <sophiabeachanel@gmail.com>
2026-09-14 21:15:35 +0800

    dquad

---
+++ core/src/mg/math/geom/Vec2.java
@@ -0,0 +1,12 @@
+package mg.math.geom;
+
+import mg.math.*;
+
+public class Vec2 {
+    private Quadruple x, y;
+
+    public Vec2(Quadruple x, Quadruple y) {
+        this.x = x;
+        this.y = y;
+    }
+}
