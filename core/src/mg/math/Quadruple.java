e3cddbf3fcdd08b684e20eab610c1d3ba83817a9
jstation <sophiabeachanel@gmail.com>
2026-09-15 18:46:54 +0800

    dquad

--- core/src/mg/math/Quadruple.java
+++ core/src/mg/math/Quadruple.java
@@ -5,7 +5,10 @@ public class Quadruple {
 
     /** ------------------------------------- */
 
-    
+    public Quadruple(long c1, long c2) {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
 
     /** ------------------------------------- */
 
